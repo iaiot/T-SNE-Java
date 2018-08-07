ca82ca6941e8a5545a22024fddfa618b68d9dd8b
haojiliang <hjl669@qq.com>
2018-08-07 17:01:33 +0800

    Create TsneThred.java

---
+++ tsne-core/src/main/java/com/jujutsu/me/TsneThred.java
@@ -0,0 +1,37 @@
+package com.me;
+
+import com.jujutsu.tsne.TSneConfiguration;
+import com.jujutsu.tsne.barneshut.BHTSne;
+import com.jujutsu.tsne.barneshut.BarnesHutTSne;
+import com.jujutsu.tsne.barneshut.ParallelBHTsne;
+
+/**
+ * TsneThred
+ *
+ * @author hjl
+ */
+public class TsneThred implements Runnable {
+
+    /**
+     * TSneConfiguration
+     */
+    private TSneConfiguration config;
+
+    public TsneThred(TSneConfiguration config) {
+        this.config = config;
+    }
+
+    @Override
+    public void run() {
+        BarnesHutTSne tsne;
+        boolean parallel = true;
+        if (parallel) {
+            tsne = new ParallelBHTsne();
+        }
+        else {
+            tsne = new BHTSne();
+        }
+
+        tsne.tsne(config);
+    }
+}
